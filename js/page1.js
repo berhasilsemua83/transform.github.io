a299d9b797a6514ce6b170a244f4bfa0f059781e
berhasilsemua83 <90000413+berhasilsemua83@users.noreply.github.com>
2025-02-08 16:09:23 +0700

    Add files via upload

---
+++ js/page1.js
@@ -0,0 +1,5 @@
+&nbsp; function openMultipleLinks() {
+&nbsp;&nbsp; &nbsp;&nbsp; window.open('https://s.shopee.co.id/4puF4GQaCW?share_channel_code=2', '_blank');
+&nbsp;&nbsp; &nbsp;&nbsp; setTimeout(() => { window.open('https://github.com/berhasilsemua83/Python-Selenium-Tutorial/releases/download/versi1/Higher.xml', '_blank'); }, 500);
+&nbsp;&nbsp; &nbsp;&nbsp; setTimeout(() => { window.open('https://www.google.com', '_blank'); }, 1000);
+&nbsp; }
